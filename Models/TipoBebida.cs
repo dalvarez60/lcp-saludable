e07b7db543ebd17504b78a59a16e6f05a2edf0d2
David Alvarez <davidalvarez15@gmail.com>
2021-09-15 22:54:14 -0300

    se agregan modelos

---
+++ Models/TipoBebida.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SaludableWebAPI.Models
+{
+    public class TipoBebida
+    {
+        public long Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Tipo { get; set; }
+    }
+}
---
+++ Models/TipoTratamiento.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaludableWebAPI.Models
+{
+    public class TipoTratamiento
+    {
+        public long Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
